1609531901447c8b82d08d0cceafd12c4cad391d
Louis Bizouerne <louis.bizouerne@lifen.fr>
2021-07-01 12:33:17 +0200

    added header

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ var corsOptions = {
   }
 
 const app = express();
-app.use(cors(corsOptions));
-app.use(express.json());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+app.use(cors(corsOptions));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     fs.writeFile('req.txt', JSON.stringify(req.query), err => {
