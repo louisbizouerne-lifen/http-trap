377845a40e411e9a08b54cd60a1992ba115490c4
Louis Bizouerne <louis.bizouerne@lifen.fr>
2021-07-01 10:01:54 +0200

    created script

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const cors = require('cors');
+const fs = require('fs')
+
+const port = 3000
+
+var corsOptions = {
+    origin: '*'
+  }
+
+const app = express();
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    fs.writeFile('req.txt', JSON.stringify(req.query), err => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        res.send('Request saved!')
+    })
+})
+
+app.post('/', (req, res) => {
+    fs.writeFile('req.txt', JSON.stringify(req.params), err => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        res.send('Request saved!')
+    })
+})
+
+app.get('/r', (req, res) => {
+    fs.readFile('req.txt', 'utf8' , (err, data) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        res.send(data)
+    })
+})
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
