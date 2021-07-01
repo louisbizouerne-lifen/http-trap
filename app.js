31f8fc44ce32cb256e0f2dbafc21ca2f1f27a191
Louis Bizouerne <louis.bizouerne@lifen.fr>
2021-07-01 10:08:37 +0200

    changed port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const fs = require('fs')
 
-const port = 3000
+const port = process.env.PORT || 3000;
 
 var corsOptions = {
     origin: '*'
